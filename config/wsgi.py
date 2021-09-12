073c133b50db9aabf70325f0a076857bac0c56d1
genImai <eichi_kite@yahoo.co.jp>
2021-09-12 17:43:56 +0900

    update20210912

--- config/wsgi.py
+++ config/wsgi.py
@@ -8,10 +8,7 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-import pymysql
 from django.core.wsgi import get_wsgi_application
-
-pymysql.install_as_MySQLdb()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
