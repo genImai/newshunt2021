bb106c38235b076564f9ac9d6d8f55a9e2573518
genImai <eichi_kite@yahoo.co.jp>
2021-09-15 19:21:46 +0900

    update20210915

--- config/settings.py
+++ config/settings.py
@@ -17,7 +17,7 @@ from django.contrib.messages import constants as messages
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["newshunt2021.herokuapp.com"]
 
@@ -150,8 +150,6 @@ ACCOUNT_FORMS = {
 ACCOUNT_ADAPTER = 'accounts.adapter.AccountAdapter'
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
-ACCOUNT_SESSION_REMEMBER = None
-
 
 SITE_ID = 15
 
